90e2853d15da2d472909a55a73e11205e9499ee4
jason2014-liu <379626076@qq.com>
2015-06-23 10:53:57 +0800

    create

---
+++ src/com/lmstudio/thrift/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * TODO
+ * @Project: thriftDemo
+ * @Title: package-info.java
+ * @Package com.lmstudio.thrift
+ * @author jason.liu
+ * @Date 2015-6-23 上午10:50:03
+ * @Version v1.0
+ */
+/**
+ * TODO
+ * @ClassName: package-info
+ * @author jason.liu
+ */
+package com.lmstudio.thrift;
